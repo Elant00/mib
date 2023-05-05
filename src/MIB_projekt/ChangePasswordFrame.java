3b506a23c88d05955a4f004f1c6fd89a0e4b9899
Elant00 <132559849+Elant00@users.noreply.github.com>
2023-05-05 14:47:06 +0200

    update

--- src/MIB_projekt/ChangePasswordFrame.java
+++ src/MIB_projekt/ChangePasswordFrame.java
@@ -29,7 +29,7 @@ public class ChangePasswordFrame extends javax.swing.JFrame {
     
     private static InfDB idb;
     /**
-     * Creates new form ChangePasswordFram
+     * Creates new form ChangePasswordFrame
      */
     public ChangePasswordFrame(InfDB iidb) {
         initComponents();
